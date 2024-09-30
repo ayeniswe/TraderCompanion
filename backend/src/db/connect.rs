cf211362843a14f18f1ec83ea2d8ed9709778e0d
Samson Ayeni <nazaritee@proton.me>
2024-09-29 22:13:28 -0500

    fix: allow linux to access db

--- backend/src/db/connect.rs
+++ backend/src/db/connect.rs
@@ -1,7 +1,12 @@
 use diesel::prelude::*;
+use std::env;
 
 pub fn establish_connection() -> SqliteConnection {
-    let database_url: &str = "app.db";
+    let database_url = if env::consts::OS == "linux" {
+        "/usr/lib/app.db"
+    } else {
+        "app.db"
+    };
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
